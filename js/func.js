5bb89b6d7385312b1dd6e2e791ab7f63354aaaa1
Eslam Mohamed <eslammohamed362@gmail.com>
2020-02-09 00:03:02 +0200

    v1.5.3

--- js/func.js
+++ js/func.js
@@ -3,78 +3,78 @@
  * @author "Eslam Mohamed"
  * @copyright "2020"
  */
-//Scroll NavBar
 
-var navBar = $('.navbar'),
+//  LOADER
+$(document).ready(function () {
+
+    setTimeout(function () {
+        $('body').addClass('loaded');
+        $('h1').css('color', '#222222');
+        $('body').css('overflow-y', 'scroll');
+    }, 3000);
+
+});
+//Scroll NavBar
+let navBar = $('.navbar'),
     data = navBar.data();
 
- 
-var scrolling = false,
+let scrolling = false,
     scrolledPast = false;
 
- 
-function switchInto() {
+switchInto = () => {
     'use strict';
     scrolledPast = true;
-   
-   
     navBar.addClass(data.intocolor);
     navBar.addClass(data.intosize);
-    console.log('into transition triggered!');
 }
 
-
-function switchStart() {
+switchStart = () => {
     'use strict';
     scrolledPast = false;
-  
-    
     navBar.removeClass(data.intocolor);
     navBar.removeClass(data.intosize);
-    console.log('start transition triggered!');
 }
 
+window.onscroll = () => { 'use strict'; return scrolling = true; };
 
-$(window).scroll(function () {'use strict'; return scrolling = true; });
-
-setInterval(function () {
+setInterval(() => {
     'use strict';
-  
+
     if (scrolling) {
-   
+
         scrolling = false;
-    
+
         if ($(window).scrollTop() > 200) {
-      
+
             if (!scrolledPast) {
                 switchInto();
             }
         } else {
- 
+
             if (scrolledPast) {
                 switchStart();
             }
         }
     }
- 
+
 }, 100);
 
 
 // AUTO SLIDE
 $('.carousel').carousel({
     interval: 3000
-  })
+})
 
 //Mobile NavBar
 
-$(document).ready(function(){
-    $(".menu-toggle").click(function(){
-      $(this).toggleClass("is-active");
-      $('.items').toggleClass("active");
+$(document).ready(function () {
+    $(".menu-toggle").click(function () {
+        $(this).toggleClass("is-active");
+        $('.items').toggleClass("active");
     });
-  });
- 
-  // //Copyright
-var copyright = document.getElementById('copy-date');
+});
+
+// //Copyright
+let copyright = document.getElementById('copy-date');
 
 copyright.textContent = new Date().getFullYear();
